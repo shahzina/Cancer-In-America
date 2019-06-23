ebe2f757adf99b0613f7e94e7d0fe1af16c5480a
Pots8 <47067112+Pots8@users.noreply.github.com>
2019-06-23 10:41:20 -0700

    Add files via upload

---
+++ datafivemort.js
@@ -0,0 +1,418 @@
+var data = [
+{
+   "Statename": "Alabama",
+   "breast_mort": 22.6,
+   "colon_rectum_mort": 16.3,
+   "lung_mort": 49,
+   "pancreas_mort": 11.5,
+   "melanoma_mort": 2.5
+ },
+ {
+   "Statename": "Alaska",
+   "breast_mort": 17.8,
+   "colon_rectum_mort": 14.4,
+   "lung_mort": 42.5,
+   "pancreas_mort": 10.4,
+   "melanoma_mort": 3.1
+ },
+ {
+   "Statename": "Arizona",
+   "breast_mort": 19,
+   "colon_rectum_mort": 12.5,
+   "lung_mort": 34.5,
+   "pancreas_mort": 10.1,
+   "melanoma_mort": 2.6
+ },
+ {
+   "Statename": "Arkansas",
+   "breast_mort": 20.1,
+   "colon_rectum_mort": 16.5,
+   "lung_mort": 57.1,
+   "pancreas_mort": 10.2,
+   "melanoma_mort": 2.5
+ },
+ {
+   "Statename": "California",
+   "breast_mort": 19.6,
+   "colon_rectum_mort": 13,
+   "lung_mort": 30,
+   "pancreas_mort": 10.2,
+   "melanoma_mort": 2.3
+ },
+ {
+   "Statename": "Colorado",
+   "breast_mort": 19,
+   "colon_rectum_mort": 11.9,
+   "lung_mort": 27.7,
+   "pancreas_mort": 8.8,
+   "melanoma_mort": 3.1
+ },
+ {
+   "Statename": "Connecticut",
+   "breast_mort": 17.2,
+   "colon_rectum_mort": 11.7,
+   "lung_mort": 34.7,
+   "pancreas_mort": 10.6,
+   "melanoma_mort": 2
+ },
+ {
+   "Statename": "Delaware",
+   "breast_mort": 21.1,
+   "colon_rectum_mort": 13.1,
+   "lung_mort": 46.5,
+   "pancreas_mort": 12.5,
+   "melanoma_mort": 3.1
+ },
+ {
+   "Statename": "District of Columbia",
+   "breast_mort": 28.5,
+   "colon_rectum_mort": 16.6,
+   "lung_mort": 31.9,
+   "pancreas_mort": 13.8,
+   "melanoma_mort": 0
+ },
+ {
+   "Statename": "Florida",
+   "breast_mort": 19,
+   "colon_rectum_mort": 12.7,
+   "lung_mort": 40,
+   "pancreas_mort": 10.5,
+   "melanoma_mort": 2.7
+ },
+ {
+   "Statename": "Georgia",
+   "breast_mort": 21.3,
+   "colon_rectum_mort": 15.6,
+   "lung_mort": 42.2,
+   "pancreas_mort": 10.9,
+   "melanoma_mort": 2.3
+ },
+ {
+   "Statename": "Hawaii",
+   "breast_mort": 19,
+   "colon_rectum_mort": 10.5,
+   "lung_mort": 31.9,
+   "pancreas_mort": 11.6,
+   "melanoma_mort": 1.6
+ },
+ {
+   "Statename": "Idaho",
+   "breast_mort": 21.9,
+   "colon_rectum_mort": 12.3,
+   "lung_mort": 34.4,
+   "pancreas_mort": 11.2,
+   "melanoma_mort": 3.6
+ },
+ {
+   "Statename": "Illinois",
+   "breast_mort": 21.8,
+   "colon_rectum_mort": 15.9,
+   "lung_mort": 43.4,
+   "pancreas_mort": 11.1,
+   "melanoma_mort": 2.2
+ },
+ {
+   "Statename": "Indiana",
+   "breast_mort": 20.6,
+   "colon_rectum_mort": 15.3,
+   "lung_mort": 50.2,
+   "pancreas_mort": 11.1,
+   "melanoma_mort": 2.7
+ },
+ {
+   "Statename": "Iowa",
+   "breast_mort": 18.5,
+   "colon_rectum_mort": 14.4,
+   "lung_mort": 45.1,
+   "pancreas_mort": 11.1,
+   "melanoma_mort": 2.5
+ },
+ {
+   "Statename": "Kansas",
+   "breast_mort": 19.8,
+   "colon_rectum_mort": 14.5,
+   "lung_mort": 44.4,
+   "pancreas_mort": 11.8,
+   "melanoma_mort": 3.1
+ },
+ {
+   "Statename": "Kentucky",
+   "breast_mort": 21.1,
+   "colon_rectum_mort": 16.5,
+   "lung_mort": 64.3,
+   "pancreas_mort": 11.5,
+   "melanoma_mort": 3.2
+ },
+ {
+   "Statename": "Louisiana",
+   "breast_mort": 21.7,
+   "colon_rectum_mort": 16.1,
+   "lung_mort": 50.5,
+   "pancreas_mort": 13.3,
+   "melanoma_mort": 1.8
+ },
+ {
+   "Statename": "Maine",
+   "breast_mort": 20.1,
+   "colon_rectum_mort": 13.8,
+   "lung_mort": 50.3,
+   "pancreas_mort": 12.5,
+   "melanoma_mort": 2.4
+ },
+ {
+   "Statename": "Maryland",
+   "breast_mort": 21.7,
+   "colon_rectum_mort": 13.5,
+   "lung_mort": 37.7,
+   "pancreas_mort": 11.7,
+   "melanoma_mort": 1.8
+ },
+ {
+   "Statename": "Massachusetts",
+   "breast_mort": 17.6,
+   "colon_rectum_mort": 11.8,
+   "lung_mort": 39.1,
+   "pancreas_mort": 11.3,
+   "melanoma_mort": 2.3
+ },
+ {
+   "Statename": "Michigan",
+   "breast_mort": 20.5,
+   "colon_rectum_mort": 14,
+   "lung_mort": 45.9,
+   "pancreas_mort": 12.1,
+   "melanoma_mort": 2.4
+ },
+ {
+   "Statename": "Minnesota",
+   "breast_mort": 19.2,
+   "colon_rectum_mort": 12.5,
+   "lung_mort": 37,
+   "pancreas_mort": 11.2,
+   "melanoma_mort": 2.4
+ },
+ {
+   "Statename": "Mississippi",
+   "breast_mort": 21.8,
+   "colon_rectum_mort": 17.8,
+   "lung_mort": 54.7,
+   "pancreas_mort": 13.8,
+   "melanoma_mort": 2.6
+ },
+ {
+   "Statename": "Missouri",
+   "breast_mort": 21.4,
+   "colon_rectum_mort": 15,
+   "lung_mort": 51.1,
+   "pancreas_mort": 11.1,
+   "melanoma_mort": 2.8
+ },
+ {
+   "Statename": "Montana",
+   "breast_mort": 21.4,
+   "colon_rectum_mort": 14,
+   "lung_mort": 39.2,
+   "pancreas_mort": 11,
+   "melanoma_mort": 2.1
+ },
+ {
+   "Statename": "Nebraska",
+   "breast_mort": 18.6,
+   "colon_rectum_mort": 15.3,
+   "lung_mort": 41.4,
+   "pancreas_mort": 9.7,
+   "melanoma_mort": 2.9
+ },
+ {
+   "Statename": "Nevada",
+   "breast_mort": 21.4,
+   "colon_rectum_mort": 15.6,
+   "lung_mort": 41.7,
+   "pancreas_mort": 10.7,
+   "melanoma_mort": 2.7
+ },
+ {
+   "Statename": "New Hampshire",
+   "breast_mort": 18.8,
+   "colon_rectum_mort": 11.4,
+   "lung_mort": 43.4,
+   "pancreas_mort": 10.4,
+   "melanoma_mort": 3
+ },
+ {
+   "Statename": "New Jersey",
+   "breast_mort": 21,
+   "colon_rectum_mort": 14.1,
+   "lung_mort": 35,
+   "pancreas_mort": 11.5,
+   "melanoma_mort": 2.1
+ },
+ {
+   "Statename": "New Mexico",
+   "breast_mort": 19.7,
+   "colon_rectum_mort": 13.5,
+   "lung_mort": 29.5,
+   "pancreas_mort": 10,
+   "melanoma_mort": 2.9
+ },
+ {
+   "Statename": "New York",
+   "breast_mort": 19.2,
+   "colon_rectum_mort": 13.1,
+   "lung_mort": 36.2,
+   "pancreas_mort": 10.7,
+   "melanoma_mort": 1.7
+ },
+ {
+   "Statename": "North Carolina",
+   "breast_mort": 21.6,
+   "colon_rectum_mort": 14.3,
+   "lung_mort": 45.6,
+   "pancreas_mort": 11.2,
+   "melanoma_mort": 2.5
+ },
+ {
+   "Statename": "North Dakota",
+   "breast_mort": 21.1,
+   "colon_rectum_mort": 12.4,
+   "lung_mort": 34.4,
+   "pancreas_mort": 11.9,
+   "melanoma_mort": 2.4
+ },
+ {
+   "Statename": "Ohio",
+   "breast_mort": 22.8,
+   "colon_rectum_mort": 15,
+   "lung_mort": 49,
+   "pancreas_mort": 12.1,
+   "melanoma_mort": 2.6
+ },
+ {
+   "Statename": "Oklahoma",
+   "breast_mort": 22.6,
+   "colon_rectum_mort": 17.9,
+   "lung_mort": 54,
+   "pancreas_mort": 10.8,
+   "melanoma_mort": 2.8
+ },
+ {
+   "Statename": "Oregon",
+   "breast_mort": 20,
+   "colon_rectum_mort": 13,
+   "lung_mort": 38.6,
+   "pancreas_mort": 12.5,
+   "melanoma_mort": 2.7
+ },
+ {
+   "Statename": "Pennsylvania",
+   "breast_mort": 21.7,
+   "colon_rectum_mort": 15,
+   "lung_mort": 43,
+   "pancreas_mort": 12.1,
+   "melanoma_mort": 2.5
+ },
+ {
+   "Statename": "Puerto Rico",
+   "breast_mort": 17.5,
+   "colon_rectum_mort": 14.1,
+   "lung_mort": 12.4,
+   "pancreas_mort": 6.9,
+   "melanoma_mort": 0.4
+ },
+ {
+   "Statename": "Rhode Island",
+   "breast_mort": 17.4,
+   "colon_rectum_mort": 12.8,
+   "lung_mort": 46.9,
+   "pancreas_mort": 11.6,
+   "melanoma_mort": 2.2
+ },
+ {
+   "Statename": "South Carolina",
+   "breast_mort": 20.4,
+   "colon_rectum_mort": 14.6,
+   "lung_mort": 45,
+   "pancreas_mort": 11.2,
+   "melanoma_mort": 2.3
+ },
+ {
+   "Statename": "South Dakota",
+   "breast_mort": 18.6,
+   "colon_rectum_mort": 15.8,
+   "lung_mort": 40.3,
+   "pancreas_mort": 10.3,
+   "melanoma_mort": 2.4
+ },
+ {
+   "Statename": "Tennessee",
+   "breast_mort": 22.1,
+   "colon_rectum_mort": 15.5,
+   "lung_mort": 54.3,
+   "pancreas_mort": 10.9,
+   "melanoma_mort": 3.1
+ },
+ {
+   "Statename": "Texas",
+   "breast_mort": 19.7,
+   "colon_rectum_mort": 13.9,
+   "lung_mort": 35,
+   "pancreas_mort": 10.2,
+   "melanoma_mort": 2
+ },
+ {
+   "Statename": "Utah",
+   "breast_mort": 20.1,
+   "colon_rectum_mort": 11.5,
+   "lung_mort": 19.1,
+   "pancreas_mort": 9.3,
+   "melanoma_mort": 3
+ },
+ {
+   "Statename": "Vermont",
+   "breast_mort": 21,
+   "colon_rectum_mort": 15.4,
+   "lung_mort": 41.5,
+   "pancreas_mort": 9.7,
+   "melanoma_mort": 2.9
+ },
+ {
+   "Statename": "Virginia",
+   "breast_mort": 21.3,
+   "colon_rectum_mort": 14.1,
+   "lung_mort": 41.2,
+   "pancreas_mort": 10.9,
+   "melanoma_mort": 2.5
+ },
+ {
+   "Statename": "Washington",
+   "breast_mort": 19.9,
+   "colon_rectum_mort": 12.5,
+   "lung_mort": 38.6,
+   "pancreas_mort": 10.8,
+   "melanoma_mort": 2.9
+ },
+ {
+   "Statename": "West Virginia",
+   "breast_mort": 21.1,
+   "colon_rectum_mort": 18.5,
+   "lung_mort": 57.3,
+   "pancreas_mort": 11.1,
+   "melanoma_mort": 3.4
+ },
+ {
+   "Statename": "Wisconsin",
+   "breast_mort": 18.7,
+   "colon_rectum_mort": 12.6,
+   "lung_mort": 39.9,
+   "pancreas_mort": 11.7,
+   "melanoma_mort": 2.8
+ },
+ {
+   "Statename": "Wyoming",
+   "breast_mort": 20.2,
+   "colon_rectum_mort": 14,
+   "lung_mort": 32.2,
+   "pancreas_mort": 10,
+   "melanoma_mort": 2.7
+ }
+]
